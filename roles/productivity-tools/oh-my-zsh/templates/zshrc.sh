479865fe7e5871c5bcc57a3fb6165ce6c61373aa
Fritz Duchardt <fritz@duchardt.net>
2024-10-06 18:28:44 +0200

    Added cluster info to prompt

--- roles/productivity-tools/oh-my-zsh/templates/zshrc.sh
+++ roles/productivity-tools/oh-my-zsh/templates/zshrc.sh
@@ -5,3 +5,4 @@ export ZSH_CUSTOM="$HOME/projects/github/fritzmyzsh"
 export ZSH_THEME="afowler"
 plugins=( {{ zsh_plugins }} )
 source $ZSH/oh-my-zsh.sh
+export RPROMPT='%F{blue} '"$(cluster-info)"'%f'
