0c8d4470f44a0eed5e6db68841872ac85c9e253c
Fritz Duchardt <fritz@duchardt.net>
2025-03-16 09:31:02 +0100

    Add syntax highlighting plugin to zsh

--- roles/productivity-tools/oh-my-zsh/templates/zshrc.sh
+++ roles/productivity-tools/oh-my-zsh/templates/zshrc.sh
@@ -1,8 +1,11 @@
-# Oh-my-zsh
+# DON'T edit manually. This comes from k8s-workstation and is rolled out with ansible
 export LOG_LEVEL=info
 export ZSH="$HOME/projects/github/ohmyzsh"
 export ZSH_CUSTOM="$HOME/projects/github/fritzmyzsh"
 export ZSH_THEME="afowler"
+
 plugins=( {{ zsh_plugins }} )
+
 source $ZSH/oh-my-zsh.sh
-export RPROMPT='%F{blue} '"$(cluster-info)"'%f'
+# shellcheck disable=SC2016
+export RPROMPT='%F{blue} $(cluster-info)%f'
